9ad2e2e6a9e408d46b9cbf48e1d7fdc5e4bff7e5
islammd39e <islammdrahat308@gmail.comm>
2022-09-12 15:22:25 +0600

    chack info

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 const getButton = (id) =>{
-    const getId = document.getElementById('id')
-    
+    const getId = document.getElementById(id)
+    return getId.value;
 }
+const productField = () =>{
+    const nameField = getButton('product-name')
+    const productField = getButton('product-quantity')
+    const intrizer = parseInt(productField)
+    // if (isNaN(nameField)) {
+    //     console.log('isstring');
+    // }else{
+    //     console.log('number');
+    // }
+    // console.log(Number.isInteger(intrizer));
+    if(!isNaN(nameField) || !Number.isInteger(intrizer)){
+        console.log('vul input');
+    }
+}
+
